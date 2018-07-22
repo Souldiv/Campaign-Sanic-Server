974029b2421d9c332e22707e9c0c705f50078943
Souldiv <amrut.phadke2016@vitstudent.ac.in>
2018-07-22 22:37:39 +0530

    fixed port issue

--- server.py
+++ server.py
@@ -2,9 +2,10 @@ from exceptions.exceptions import *
 from handlers.handlers import *
 from app import app
 import asyncio
+import os
 
 if __name__ == '__main__':
-    server = app.create_server(host="0.0.0.0", port=6969)
+    server = app.create_server(host="0.0.0.0", port=int(os.environ.get('PORT',6969)))
     loop = asyncio.get_event_loop()
     task = asyncio.ensure_future(server)
     loop.run_forever()
